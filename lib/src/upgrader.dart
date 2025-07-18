4b3bb153e47cb41c986d10d02a018f60c8bcad86
hai <vusonhai8888@gmail.com>
2025-07-18 17:34:09 +0700

    fix ui ins

--- lib/src/upgrader.dart
+++ lib/src/upgrader.dart
@@ -747,7 +747,7 @@ class Upgrader with WidgetsBindingObserver {
       title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Color(0xff222222)),),
       content: Padding(
         padding: const EdgeInsets.only(top: 8.0),
-        child: content,
+        child: Text(message, style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 14, color: Color(0xff222222)),),
       ),
       actions: [
         Column(
