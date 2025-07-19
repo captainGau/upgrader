3c83eda9f13ce4c4e1ca73c3a2a7e21092de6d38
hai <vusonhai8888@gmail.com>
2025-07-19 16:46:07 +0700

    fix ui ins

--- lib/src/upgrader.dart
+++ lib/src/upgrader.dart
@@ -744,6 +744,9 @@ class Upgrader with WidgetsBindingObserver {
     return cupertino
         ? (useCupertinoVerticalButtons ?? false
         ? AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
       title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Color(0xff222222)), textAlign: TextAlign.center,),
       content: Text(message, style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 14, color: Color(0xff222222)), textAlign: TextAlign.center,),
       actions: [
